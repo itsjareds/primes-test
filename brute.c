2364ca85272e236b73d22bf10074447e208630f5
Qiaosong Dong <qdong@imp2.cs.clemson.edu>
2015-04-15 12:36:23 -0400

    Added execution time output and exit method to brute.c, minor bug fix.

--- brute.c
+++ brute.c
@@ -5,56 +5,57 @@
 
 int main(int argc, char *argv[])
 {
-  int size, rank;
-  double t, start;
-  MPI_Status status;
-  MPI_Init(&argc, &argv);
-  int i, j;
-  int count = 2;
-  int result = 1;
-  start = MPI_Wtime();
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+int size, rank;
+double t, start;
+MPI_Status status;
+MPI_Init(&argc, &argv);
+int i;
+int count = 2;
+int result = 1;
+start = MPI_Wtime();
+MPI_Comm_size(MPI_COMM_WORLD, &size);
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  long random = 2801763489;
+long random = 100000007;
 
-  if (rank == 0){
-    for (i=1;i<size && count<random;i++) {
-      j = i + 1;
-      MPI_Send(&j, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-      count++;
-    }
-    while(count<random) {
-      MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-      if(result == 0) break;
-      MPI_Send(&count, 1, MPI_INT, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
-      count++;
-    }
+if (rank ==0){
+  while(count < random){
+  for (i=1;i<size;i++){
+  if (count < random){
+    MPI_Send(&count, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+    count++;
+    MPI_Recv(&result, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+      if(result == 0){
+  printf("\ncount:%d\n",count);
+        printf("It's not a prime");
+        t = MPI_Wtime() - start;
+        printf("\ntotal execution time: %f\n",t);
+        MPI_Finalize();
+        exit(0);
+      }
   }
-
-  else{
-    while(1) {
-      MPI_Recv(&count, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-      if (count == -1) break;
-      if (random == count)
-        result = 1;
-      else if (random % count == 0)
-        result = 0;
-      MPI_Send(&result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    }
-  }
-
-  if (rank == 0) {
-    printf("It's %s prime\n", result?"a":"not a");
+  else {
+    printf("done\n");
     t = MPI_Wtime() - start;
-
-    j = -1;
-    for (i=1;i<size;i++) {
-      MPI_Send(&j, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-    }
-
-    printf("total execution time: %f\n",t);
+    printf("\ntotal execution time: %f\n",t);
+    MPI_Finalize();
+    exit(0);
   }
-  MPI_Finalize();
-  return 0;
+  }
+  }
+}
+
+else{
+  while(count < random){
+  MPI_Recv(&count, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+  if (random % count == 0) result = 0;
+  else result = 1;
+  MPI_Send(&result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+  }
+}
+printf("Finish");
+t = MPI_Wtime() - start;
+printf("\ntotal execution time: %f\n",t);
+MPI_Finalize();
+return 0;
 }
